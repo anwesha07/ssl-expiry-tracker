a6a00234c394124c56e2dc900c4601bfcfd33e07
Anwesha <iamanwesha17@gmail.com>
2023-07-15 19:53:31 +0530

    Add console log in cron

--- pages/api/notify.ts
+++ pages/api/notify.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log('Hello cron!');
   res.status(200).send('Hello Cron!');
 }
