c59ceb485d5e3df767bd28b8cef5c5137eedde51
Anwesha <iamanwesha17@gmail.com>
2023-07-15 19:32:22 +0530

    cron job test

--- pages/api/alertUser.ts
+++ pages/api/alertUser.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).end('Hello Cron!');
+  res.status(200).send('Hello Cron!');
 }
